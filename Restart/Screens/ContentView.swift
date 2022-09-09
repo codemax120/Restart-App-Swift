abd118c1d223d6dfad5e2a9e04ebd9acbebe170b
Max Herrera <harvey.herrera@lumation.co>
2022-09-09 12:32:51 -0600

    Added implementation of onboarding with parallax effect, taking into account the importance of maintaining the user experience when navigating between Swift UI components.

--- Restart/Screens/ContentView.swift
+++ Restart/Screens/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @AppStorage("onboarding") var isOnBoardingViewActive: Bool = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            if isOnBoardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
     }
 }
 
